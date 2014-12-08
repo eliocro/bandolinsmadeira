05c1e45ebc46061ce4ccf332d5d355607fdd1c19
Élio Cró <elio@front.no>
2014-12-08 15:15:22 +0000

    Moved theme files to uploads

--- wp-content/themes/piano-black/img/back.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$image_number = date("z")%6+1;
-	$image = @imagecreatefromjpeg("back_".$image_number.".jpg");
-	header ('Content-Type: image/jpeg');
-    imagejpeg($image, "", 90);  
-	die();
-?>
